1505d6d8531c6cf47f62478b191f3c0d912f5152
Christina Cox <33332810+chopec79@users.noreply.github.com>
2020-05-08 15:33:58 -0500

    class ended

---
+++ average loop.py
@@ -0,0 +1,11 @@
+count = 0
+sum = 0
+print('Before', count, sum)
+for zork in [9, 41, 12, 3, 74, 15] :
+    count = count + 1
+    sum = sum + zork
+    print(count, sum, zork)
+print('After', 'Count', 'Sum', 'Average')
+print ('end',count,sum, sum/count)
+
+    
