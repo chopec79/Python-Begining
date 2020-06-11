14574c89f439eae96da330a6dc40fc8bf5b47755
Christina Cox <33332810+chopec79@users.noreply.github.com>
2020-06-11 13:56:20 -0500

    more school files

--- String.py
+++ String.py
@@ -4,4 +4,3 @@ while index < len(fruit):
     letter = fruit[index]
     print(index, letter)
     index = index +1
-    
---
+++ test_filePY.py
@@ -0,0 +1,3 @@
+X= open(C:\Users\ccox100\Documents\GitHub\Python-Begining\Test_txt\TEST1.TXT)
+print(x)
+
